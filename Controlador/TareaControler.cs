c7049ec46e9970d32ef597ee2efaa891e72704f5
Pablo <pablovicente19@hotmail.com>
2024-09-09 15:08:05 -0300

    Terminar el diseño

--- Controlador/TareaControler.cs
+++ Controlador/TareaControler.cs
@@ -1,6 +1,33 @@
-﻿namespace Controlador
+﻿using Modelo;
+namespace Controlador
 {
     public class TareaControler
     {
+        private readonly Modelo.AppContext _context;
+
+        public TareaControler()
+        {
+            _context = new Modelo.AppContext();
+        }
+
+        public List<Tarea> ObtenerTareas()
+        {
+            return new List<Tarea>();
+        }
+        public void AgregarTarea(Tarea tarea) 
+        {
+            _context.Add(tarea);
+            _context.SaveChanges();
+        }
+        public void ModificarTarea(Tarea tarea) 
+        {
+            _context.Update(tarea);
+            _context.SaveChanges();
+        }
+        public void EliminarTarea(Tarea tarea) 
+        {
+            _context.Remove(tarea);
+            _context.SaveChanges();
+        }
     }
 }
---
+++ Modelo/AppContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class AppContext : DbContext
+    {
+        private DbSet<Tarea> Tareas {  get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=");
+            //optionsBuilder.UseSqlite("Data Source=todo.db");
+        }
+    }
+}
