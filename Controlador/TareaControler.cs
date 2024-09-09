fec22ebe842478e8796388c7fad2c909e1b2f177
Pablo <pablovicente19@hotmail.com>
2024-09-09 14:38:29 -0300

    Agregar archivos de proyecto.

---
+++ Controlador/TareaControler.cs
@@ -0,0 +1,6 @@
+﻿namespace Controlador
+{
+    public class TareaControler
+    {
+    }
+}
---
+++ Modelo/Tarea.cs
@@ -0,0 +1,11 @@
+﻿namespace Modelo
+{
+    public class Tarea
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public bool Completed { get; set; }
+        public DateTime CreateAt { get; set; } = DateTime.Now;
+    }
+}
